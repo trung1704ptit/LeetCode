9bf4b06561c12dfea33a98a4e334be96e8406095
Trung Nguyen <trung1704ptit@gmail.com>
2021-10-16 15:22:55 +0700

    add 387 first unique character

---
+++ HashTable/387-first-unique-character-in-a-string.py
@@ -0,0 +1,29 @@
+"""
+https://leetcode.com/problems/first-unique-character-in-a-string/
+
+Questions:
+- Is that string contains lowercase or upercase?
+- Is that uinique if a and A appear in string?
+
+Solutions:
+- Using hashmap to store the number counter of each character
+- if string count equal 1 => return index
+"""
+
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = collections.Counter(s)
+        print(count)
+        
+        for i, c in enumerate(s):
+            if count[c] == 1:
+                return i
+        return -1
+    
+# Time: O(n) loop through string 2 time
+# space: O(1) because the string is alphabet with 26 characters 
