b90c8da9b3bd63bece2b80df466d880f6e439737
Trung Nguyen <trung1704ptit@gmail.com>
2021-11-01 23:19:03 +0700

    add comment 81

--- BinarySearch/81-search-in-rotated-sorted-array-ii.py
+++ BinarySearch/81-search-in-rotated-sorted-array-ii.py
@@ -14,21 +14,25 @@ class Solution(object):
         :rtype: bool
         """
 
-
-        left, right = 0, len(nums) - 1
-        while left <= right :
+        left, right = 0, len(nums) -1
+        while left <= right:
             mid = left + (right - left)/2
-            # if target is the mid, return true
             if nums[mid] == target:
                 return True
             if nums[mid] == nums[left]:
-                left += 1
-            elif nums[mid] > nums[left] :
-                if nums[mid] > target and nums[left] <= target :
+                left +=1
+            elif nums[mid] > nums[left]:
+                # two case:
+                if nums[mid] > target and nums[left] <= target:
+                    # target maybe now in left of mid
                     right = mid - 1
-                else : left = mid + 1
-            else :
-                if nums[mid] < target and nums[right] >= target :
+                else:
+                    # target maybe now in right of mid
+                    left =  mid + 1
+            else:
+                # two case
+                if nums[mid] < target and nums[right] >= target:
                     left = mid + 1
-                else : right = mid - 1
+                else:
+                    right = mid - 1
         return False
