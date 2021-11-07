ff205868bc1784026a9d240865b7465d065e9487
Trung Nguyen <trung1704ptit@gmail.com>
2021-11-07 09:35:11 +0700

    add 2 pointers

---
+++ 2Pointers/845-longest-mountain-in-array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def longestMountain(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        res = 0
+        s = 0
+        while s < len(arr):
+            f = s
+            while f + 1 < len(arr) and arr[f] < arr[f+1]:
+                f += 1
+            # m is mountain
+            m = f
+            while m + 1 < len(arr) and arr[m] > arr[m+1]:
+                m += 1
+            
+            if f != m and s != f:
+                res = max(res, m-s+1)
+            
+            if s != m:
+                s = m
+            else:
+                s += 1
+                
+        return res
+            
+        
---
+++ BinarySearchTree/669-trim-a-binary-search-tree.py
@@ -0,0 +1,32 @@
+"""
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def trimBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return root
+        
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+        
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
+        
+        return root
+        
