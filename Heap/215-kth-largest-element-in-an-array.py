611088168c3f0a75a9bf94416e06824a66f57520
Trung Nguyen <trung1704ptit@gmail.com>
2021-11-13 21:42:32 +0700

    add 215 using heap

---
+++ Heap/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,33 @@
+"""
+https://leetcode.com/problems/kth-largest-element-in-an-array
+Solution:
+Using min-heap
+build and min-heap from 0->k of list
+loop k => size
+if current element > root, relace root with current element,
+else continue
+return the root of min-heap
+"""
+import heapq
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        # base case
+        if not nums or len(nums) < k:
+            exit(-1)
+        
+        # build min-heap with nums of nodes: k nodes
+        pq = nums[0:k]
+        heapq.heapify(pq)
+
+        # do for the remaining list elements
+        for i in range(k, len(nums)):
+            # if the current element greater than root of heap:
+            if nums[i] > hq[0]:
+                # pop the smallest item and push the new item into heap, heap size doesn't change.
+                heapq.heapreplace(pq, nums[i])
+
+        # return the root of heap, that is the k largest element
+        return pq[0]
+
+
+        
