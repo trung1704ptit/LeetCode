4c1a47dcd967a074c7fc6414605ccc1dcf3402be
Trung Nguyen <trung1704ptit@gmail.com>
2021-12-12 15:34:22 +0700

    update 139

--- BFS/139-word-break.py
+++ BFS/139-word-break.py
@@ -22,15 +22,10 @@ class Solution(object):
             if start == len(s): return True
             if start in seen: continue
                 
-            print(seen)
-            
             for end in range(start+1, len(s) + 1):
-                print(s[start:end])
                 if s[start:end] in wordDictSet:
                     q.append(end)
             
             seen.add(start)
-            
-        print(seen)
             
         return False
