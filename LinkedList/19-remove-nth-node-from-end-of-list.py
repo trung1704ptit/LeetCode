573aa2999228ac28611b88c9dcba4c132357cb7d
Trung Nguyen <trung1704ptit@gmail.com>
2021-12-21 12:21:54 +0700

    update 19

--- LinkedList/19-remove-nth-node-from-end-of-list.py
+++ LinkedList/19-remove-nth-node-from-end-of-list.py
@@ -10,3 +10,41 @@ Using two pointers
 - At that point, we will remove the node and link it to the next of current node.
 """
 
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        
+        # using two pointers         
+        slow =  head
+        fast =  head
+        
+        # move fast to the nth of list first         
+        for i in range(0, n):
+            # if n is equal the number of nodes
+            if fast.next is None:
+                if i == n - 1:
+                    head = head.next
+                return head
+            
+            fast = fast.next
+        
+        while fast.next is not None:
+            slow = slow.next
+            fast = fast.next
+            
+        # now fast is end of list, slow in nth position
+        if slow.next is not None:
+            slow.next = slow.next.next
+            
+        return head
+            
