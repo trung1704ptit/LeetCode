94fef6373a4cfbcaad3fe5a4584e1280960f6ac6
Trung Nguyen <trung1704ptit@gmail.com>
2022-01-11 16:33:18 +0700

    add 113

--- Others/56-merge-intervals.py
+++ Others/56-merge-intervals.py
@@ -8,8 +8,8 @@ class Solution(object):
 
         for interval in intervals[1:]:
             print(result)
-            // result[-1] is the last item of result
-            // result[-1][1] is the second value of last item in result
+            # result[-1] is the last item of result
+            # result[-1][1] is the second value of last item in result
             if interval[0] <= result[-1][1]:
                 result[-1][1] = max(result[-1][1], interval[1])
             else:
