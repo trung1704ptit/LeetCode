a07319f7d15a1972f5d111a53b3221837a812e2d
Trung Nguyen <trung1704ptit@gmail.com>
2022-01-18 21:52:44 +0700

    add 713

--- 2Pointers/713-subarray-product-less-than-k.py
+++ 2Pointers/713-subarray-product-less-than-k.py
@@ -27,3 +27,31 @@ class Solution(object):
                 else:
                     break
         return count
+
+
+
+# ----------------------------------------------------
+
+
+class Solution(object):
+    def numSubarrayProductLessThanK(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if k <= 1:
+            return 0
+        
+        prod = 1
+        count = 0
+        left = 0
+        for right, n in enumerate(nums):
+            prod *= nums[right]
+            while prod >= k:
+                prod = prod // nums[left]
+                left +=1
+
+            count += right - left + 1
+            
+        return count
